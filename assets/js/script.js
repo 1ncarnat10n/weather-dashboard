96884aeac5bf1582e3bc844576ae6643c4953786
BasedAtlas <tsaiwilliam888@gmail.com>
2023-09-28 13:32:46 -0700

    Added some api functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,11 @@ var windEl = document.getElementById("wind");
 var humidityEl = document.getElementById("humidity");
 
 var dayEl = [];
-var dayEl = [];
-var dayEl = [];
-var dayEl = [];
-var dayEl = [];
-var dayEl = [];
+var dayDateEl = [];
+var dayIconEl = [];
+var dayTempEl = [];
+var dayWindEl = [];
+var dayHumidityEl = [];
 
 for (x = 0, y = 1; x < 5; x++, y++) {   
     dayEl[x] = document.getElementById("day-" + y);
@@ -33,3 +33,90 @@ var weatherIconUrl1 = "https://openweathermap.org/img/wn/";
 var weatherIconUrl2 = "@2x.png";
 
 var previousUserInput = JSON.parse(localStorage.getItem("previousUserInput"));
+
+function init() {
+    console.log(previousUserInput);
+    if (!previousUserInput) {
+        console.log(!previousUserInput);
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=ohio&appid=0c4a0f7b9dff27d58bfb79aaa0d50f4c&units=metric")
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                    displayCurrentWeather(data);
+                    previousUserInput = [" "];
+                    localStorage.setItem("previousUserInput", JSON.stringify(previousUserInput));
+                })
+            }
+            else {
+                alert("Error: " + response.status);
+            }
+        })
+    
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q=ohio&appid=0c4a0f7b9dff27d58bfb79aaa0d50f4c&units=metric")
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                    displayFutureWeather(data);
+                })
+            }
+        })
+    }
+    else {
+        // display lastest search
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + previousUserInput[0] + "&appid=0c4a0f7b9dff27d58bfb79aaa0d50f4c&units=metric")
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                    displayCurrentWeather(data);
+                    displayHistory();
+                })
+            }
+            else {
+                alert("Error: " + response.status);
+            }
+        })
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + previousUserInput[0] + "&appid=0c4a0f7b9dff27d58bfb79aaa0d50f4c&units=metric")
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                    displayFutureWeather(data);
+                })
+            }
+        })
+    }
+}
+
+function findAPI() {
+    var userInput = userInputEl.value;
+    userInputEl.value = "";
+
+    console.log(userInput);
+    
+    fetch(apiUrl + userInput + "&appid=" + apiKey + "&units=metric")
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                displayCurrentWeather(data);
+                historyTab(userInput);
+            })
+        }
+        else {
+            alert("Error: " + response.status);
+        }
+    })
+
+    fetch(apiUrl2 + userInput + "&appid=" + apiKey + "&units=metric")
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                displayFutureWeather(data);
+            })
+        }
+    })
+}
